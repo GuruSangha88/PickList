4fab526089bf161ee9ce53d0f1a6fd78e07cf4de
GuruSangha88 <gurwinder@trigvent.com>
2025-04-30 14:53:34 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PicklistComponent } from './components/picklist/picklist.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, PicklistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
